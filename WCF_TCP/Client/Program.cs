1674d3f6c93cead145035bd8cb3023d8d2b55fd2
yongliangli <yongliangli@vm2016>
2018-03-08 20:01:58 -0600

    Add wcf sample

---
+++ WCF_TCP/Client/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Fabric;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using WcfInterface;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallWcf();
+            Console.ReadKey();
+        }
+
+        static void CallWcf()
+        {
+            var ServiceUri = new Uri("fabric:/Application3/Stateful1");
+            // Create binding
+            Binding binding = WcfUtility.CreateTcpClientBinding();
+
+            ServicePartitionResolver servicePartitionResolver = new ServicePartitionResolver(() => new FabricClient());
+
+            // Create a partition resolver
+            IServicePartitionResolver partitionResolver = ServicePartitionResolver.GetDefault();
+            // create a  WcfCommunicationClientFactory object.
+            var wcfClientFactory = new WcfCommunicationClientFactory<ICalculator>
+                (clientBinding: binding, servicePartitionResolver: servicePartitionResolver);
+
+            //
+            // Create a client for communicating with the ICalculator service that has been created with the
+            // Singleton partition scheme.
+            //
+            var calculatorServiceCommunicationClient = new WcfCommunicationClient(
+                            wcfClientFactory,
+                            ServiceUri,
+                            new ServicePartitionKey(1));
+
+            //
+            // Call the service to perform the operation.
+            //
+            Stopwatch stopwatch = new Stopwatch();
+            var total = 0.0;
+            for(int i = 0; i < 1000; i++)
+            {
+                stopwatch.Restart();
+                var result = calculatorServiceCommunicationClient.InvokeWithRetryAsync(
+                           client => client.Channel.Add(2, 3)).Result;
+                stopwatch.Stop();
+                var used = stopwatch.Elapsed.TotalMilliseconds;
+                total += used;
+
+                Console.WriteLine($"{i + 1} used: {used}ms, avg:{total / (i + 1)}, result:{result}");
+
+            }
+
+           
+
+        }
+    }
+}
---
+++ WCF_TCP/Stateful1/Stateful1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Fabric;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Data.Collections;
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Communication.Wcf;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+using WcfInterface;
+
+namespace Stateful1
+{
+    /// <summary>
+    /// An instance of this class is created for each service replica by the Service Fabric runtime.
+    /// </summary>
+    internal sealed class Stateful1 : StatefulService, ICalculator
+    {
+        public Stateful1(StatefulServiceContext context)
+            : base(context)
+        { }
+
+        public Task<int> Add(int value1, int value2)
+        {
+            return Task.FromResult(value1 + value2);
+        }
+
+        /// <summary>
+        /// Optional override to create listeners (e.g., HTTP, Service Remoting, WCF, etc.) for this service replica to handle client or user requests.
+        /// </summary>
+        /// <remarks>
+        /// For more information on service communication, see https://aka.ms/servicefabricservicecommunication
+        /// </remarks>
+        /// <returns>A collection of listeners.</returns>
+        protected override IEnumerable<ServiceReplicaListener> CreateServiceReplicaListeners()
+        {
+            return new[] { new ServiceReplicaListener((context) =>
+        new WcfCommunicationListener<ICalculator>(
+            wcfServiceObject:this,
+            serviceContext:context,
+            //
+            // The name of the endpoint configured in the ServiceManifest under the Endpoints section
+            // that identifies the endpoint that the WCF ServiceHost should listen on.
+            //
+            endpointResourceName: "WcfServiceEndpoint",
+
+            //
+            // Populate the binding information that you want the service to use.
+            //
+            listenerBinding: WcfUtility.CreateTcpListenerBinding()
+        )
+    )};
+        }
+
+        /// <summary>
+        /// This is the main entry point for your service replica.
+        /// This method executes when this replica of your service becomes primary and has write status.
+        /// </summary>
+        /// <param name="cancellationToken">Canceled when Service Fabric needs to shut down this service replica.</param>
+        protected override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            // TODO: Replace the following sample code with your own logic 
+            //       or remove this RunAsync override if it's not needed in your service.
+
+            var myDictionary = await this.StateManager.GetOrAddAsync<IReliableDictionary<string, long>>("myDictionary");
+
+            while (true)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                using (var tx = this.StateManager.CreateTransaction())
+                {
+                    var result = await myDictionary.TryGetValueAsync(tx, "Counter");
+
+                    ServiceEventSource.Current.ServiceMessage(this.Context, "Current Counter Value: {0}",
+                        result.HasValue ? result.Value.ToString() : "Value does not exist.");
+
+                    await myDictionary.AddOrUpdateAsync(tx, "Counter", 0, (key, value) => ++value);
+
+                    // If an exception is thrown before calling CommitAsync, the transaction aborts, all changes are 
+                    // discarded, and nothing is saved to the secondary replicas.
+                    await tx.CommitAsync();
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            }
+        }
+    }
+}
